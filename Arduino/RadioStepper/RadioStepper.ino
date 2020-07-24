e51a319b1161966e49378d89ee948ca74b55901f
Enrico <enrico.miglino@gmail.com>
2020-07-24 13:17:32 +0200

    Final version. Implemented tuner programming and the looper control

--- Arduino/RadioStepper/RadioStepper.ino
+++ Arduino/RadioStepper/RadioStepper.ino
@@ -1,11 +1,11 @@
 /**
  * @file RadioStepper.ino
- * @brief Software sketch to contorl radio tuner with a stepper motor via a rotary encoder
+ * @brief Software sketch to contorl a radio tuner with a stepper motor via a rotary encoder
  * with Arduino Nano.
  * 
  * @author Enrico Miglino <balearicdynamics@gmail.com>
- * @date July2020
- * @version 1.0 Nano build 2
+ * @date July 2020
+ * @version 1.0 Nano build 10
  */
 
 #include <Streaming.h>
@@ -14,8 +14,8 @@
 #include <Stepper.h>
 #include "constants.h"
 
-//! Undef this constant to eidable the serial output debug
-#define DEBUG
+//! Undef this constant to enable the serial debug
+#undef DEBUG
 
 //! Pointer to the ClickEncoder class
 ClickEncoder *encoder;
@@ -28,15 +28,6 @@ Stepper radioTuner(STEPS_PER_REV, STEPPER_PIN_1,
 void timerIsr() {
   encoder->service();
 }
-
-//! Bank selection parameters
-struct RadioStepper {
-  //! The starting position has been selected
-  bool isSelected = false;
-  //! Roatry encoder last position
-  //! Current rotary encoder position
-  int16_t encValue;
-};
 
 RadioStepper radioStepper;
 
@@ -59,6 +50,12 @@ void setup() {
   // Initialize the Rotary Encoder
   Timer1.initialize(1000);
   Timer1.attachInterrupt(timerIsr); 
+
+  // ------------------------------------------------
+  // Enable the loop control interrupt button
+  // ------------------------------------------------
+  pinMode(LOOPER_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(LOOPER_BUTTON), irqLoopButton, LOW);
 }
 
 /** 
@@ -76,39 +73,133 @@ void loop() {
 
   // Check if the rotary postion has changed (exclude the zero status
   if ( (radioStepper.encValue != 0 ) && (encoderCounter == ENCODER_READINGS)) {
+    if(radioStepper.isProgrammed == true) {
+      // If the tuner is programmed and the user moves the rotary encoder
+      // the programmed status is automatically reset
+      setProgrammingStatus(false);
+    }
     // Disable the bank selection until the user does not press the rotary encoder button
     encoderCounter = 0; // Reset che counter readings
-    radioStepper.isSelected = false;
     // Check for the direction (clockwise of conterclockwise)
     if (radioStepper.encValue == ROTARY_CW) {
       #ifdef DEBUG
       Serial << "Clockwise rotation " << ONE_MOVE_CLOCKWISE << endl;
       #endif
       radioTuner.step(ONE_MOVE_CLOCKWISE);
+      // Update the loop counter (only if programming is set)
+      updateLoopCount(ONE_MOVE_CLOCKWISE);
     } // Clockwise rotation 
     else {
       #ifdef DEBUG
       Serial << "Counterclockwise rotation " << ONE_MOVE_COUNTERCLOCKWISE << endl;
       #endif
       radioTuner.step(ONE_MOVE_COUNTERCLOCKWISE);
+      // Update the loop counter (only if programming is set)
+      updateLoopCount(ONE_MOVE_COUNTERCLOCKWISE);
       } // Counterclockwise rotation
   } // Rotary encoder has been moved twice
   else {
     if(radioStepper.encValue != 0){
       encoderCounter++;
-        #ifdef DEBUG
-        Serial << "First encoder reading, ignored." << endl;
-        #endif
     }
   } // First encoder reading
 
   // Check for the rotary encoder button press. The 0 value shown on power-on can't be selected
   ClickEncoder::Button encButton = encoder->getButton();
-  if( (encButton == ClickEncoder::Clicked) && 
-      (radioStepper.isSelected == false) ) {
-    #ifdef DEBUG
-    Serial << "BUTTON PRESSED " << endl;
-    #endif
+  if(encButton == ClickEncoder::Clicked) {
+    if(radioStepper.isSelected == false){
     radioStepper.isSelected = true;
+    setProgrammingStatus(false);
+    #ifdef DEBUG
+    Serial << "Start freq. range programming" << endl;
+    #endif
+    } // Button pressed for the first time: start programming the range
+    else {
+      setProgrammingStatus(true);
+    } // Button pressed for the second time. Programming ended.
+  }
+
+  // Check for looping
+  if(radioStepper.isLooping == true) {
+    radioStepper.tunerPosition += (STEPPER_INCREMENT * radioStepper.loopDirection);
+    // Check if the direction should be inverted
+    if( (radioStepper.tunerPosition == 0) || (radioStepper.tunerPosition == radioStepper.loopSteps) ) {
+      radioStepper.loopDirection *= -1; // Invert the loop direction
+    }
+    radioTuner.step(STEPPER_INCREMENT * radioStepper.loopDirection);
+    #ifdef DEBUG
+    Serial << "Current stepper position " << radioStepper.tunerPosition << endl;
+    #endif
+  }
+}
+
+/**
+ * IRQ Vector callback for Nano IRQ 0 (the loop control button pin)
+ */
+void irqLoopButton() {
+  if(radioStepper.isProgrammed == true) {
+    // If the tuner is programmed, change the status of the loop flag
+    if(radioStepper.isLooping == true) {
+      radioStepper.isLooping = false;
+    } else {
+      radioStepper.isLooping = true;
+    }
+  }
+}
+
+/**
+ * Change the radioStepper settings depending on
+ * the program mode.
+ * 
+ * \param mode Enable or diable the programming mode
+ */
+ void setProgrammingStatus(boolean mode) {
+  if(mode == true) {
+      #ifdef DEBUG
+      Serial << "End prog. Tuner loops " << radioStepper.loopSteps <<
+                " steps in both directions" << endl;
+      #endif
+      // reset the programming selection status for the next cycle
+      radioStepper.isSelected = false;
+      radioStepper.isProgrammed = true;
+      // Set the starting point to the last position reached while programming
+      radioStepper.tunerPosition = radioStepper.loopSteps;
+      // Enable the looping mode (temporary for testing only)
+      radioStepper.isLooping = true;
+      if(radioStepper.loopSteps > 0) {
+        // Start decreasing the tuner position
+        radioStepper.loopDirection = ROTATION_CCW;
+      } else {
+        // Start increasing the tuner position
+        radioStepper.loopDirection = ROTATION_CW;
+      }
+  } else {
+      #ifdef DEBUG
+      Serial << "Prog mode off" << endl;
+      #endif
+    //! Initializes the steps counter
+    radioStepper.loopSteps = 0;
+    // Disable the programming status
+    radioStepper.isProgrammed = false;
+    // Disable the looping mode
+    radioStepper.isLooping = false;
+  }
+ }
+
+/**
+ * Update the steps loop count according to the 
+ * rotary encoder direction.
+ * 
+ * The counter is updated only when the system is 
+ * in programming mode.
+ * 
+ * \param dir The number of steps, positive or negative.
+ */
+ void updateLoopCount(int dir) {
+  if(radioStepper.isSelected == true) {
+    radioStepper.loopSteps += dir;
+    #ifdef DEBUG
+    Serial << "Update the loop counter to " << radioStepper.loopSteps << endl;
+    #endif
   }
 }
--- Arduino/RadioStepper/constants.h
+++ Arduino/RadioStepper/constants.h
@@ -14,8 +14,9 @@
 #define STEPS_PER_REV 200
 //! Preset tuner stepper speed
 #define STEPPER_SPEED 90
-//! Number of steps per increment.
-#define STEPPER_INCREMENT 200
+//! Number of steps per increment, corresponding to a rotary encoder
+//! single step.
+#define STEPPER_INCREMENT 100
 //! Stepper direction clockwise. The number of increments is multiplied
 //! by the direction to get a positive or negative number
 #define ROTATION_CW 1
@@ -41,7 +42,7 @@
 //        Rotary Encoder
 // ==============================
 
-//! Rotary encoder button pin
+//! Rotary encoder button pin (attached to Nano IRQ1)
 #define ROTARY_BUTTON 3
 //! Rotary encoder clock pin
 #define ROTARY_CLK 6
@@ -55,5 +56,56 @@
 //! Number of reading of the rotary encoder to take effect
 //! Only the second reading is considered valid
 #define ENCODER_READINGS 1
+
+// ==============================
+//        Looper Button
+// ==============================
+
+//! Rotary encoder button pin (attached to Nano IRQ0)
+#define LOOPER_BUTTON 2
+
+// ==============================
+//        Status Struct
+// ==============================
+
+/**
+ * The RadioStepper structure contains the status of
+ * all the parameters controlling the behavior of the radio
+ */
+struct RadioStepper {
+  /** 
+   * The starting position has been selected 
+   * 
+   * This happens when the user press for the first
+   * time the rotary encoder button. From that point,
+   * the number of effective steps is counted until
+   * the button is not pressed for the second time.
+   */
+  bool isSelected = false;
+  /**
+   * When the button has been pressed for the second time, the 
+   * programmed status indicates that the system is ready to 
+   * loop the tuner.
+   */
+  bool isProgrammed = false;
+  //! Status enabled when the tuner is looping
+  bool isLooping = false;
+  //! Current relative tuner position inside a loop
+  int tunerPosition = 0;
+  //! Looping direction. It is inverted when one of the two limits
+  //! is reached
+  int loopDirection = 0;
+  /**
+   * Steps units expressed in number of rotary pulses
+   * 
+   * The units are added algebraically to the counter until
+   * the rotary encoder button is not pressed for the second
+   * time. At this point the controller is programmed to execute
+   * a loop.
+   */
+  int loopSteps = 0;
+  //! Current rotary encoder position
+  int16_t encValue = 0;
+};
 
 #endif
