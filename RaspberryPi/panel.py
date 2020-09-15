403fdafaa1fc845426e25e7df871a71ac916b92a
Enrico <enrico.miglino@gmail.com>
2020-09-15 17:57:57 +0200

    Implemented the parametrization of buttons

--- RaspberryPi/panel.py
+++ RaspberryPi/panel.py
@@ -9,72 +9,60 @@
 # effects generators and more.
 #
 # @author Enrico Miglino <balearicdynamicw@gmail.com>
-# @version 1.0 build 1
+# @version 1.0 build 2
 # @date September 2020
 
 import tkinter as tk
 import numpy as np
 from functools import partial
 from time import sleep
+from PIL import Image, ImageTk
 
-# Buttons grid size, rows
-panel_rows = 8
-# Buttons grid size, columns
-panel_cols = 14
-# Number of images (shaded colors) a button can have
-max_button_images = 8
-# The size of the square buttons
-button_size = 46
-# The max number of fucntion groups (associated to the same number of
-# button colors)
-max_functions = max_button_images
-
-# Creation of the root GUI
+# ------------------------ Creation of the root GUI
 window = tk.Tk()
 window.state('normal')
 window.title('Py Synth Control panel')
 window.background = 'black'
 
-# A frame that includes all the buttons.
-# The parameters for the border and pads will center the button grid
-# on the screen. Keep them fixed! Should be recalculated if the
-# button size or number of buttons change.
-frame_container = tk.Frame(
-    window,
-    highlightcolor='black',
-    highlightbackground='black',
-    background="black",
-    border=10,
-    padx = 24,
-    pady = 6
-)
+# ------------------------ Screen resolution to parametrize the size of the buttons
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
 
-# Pack the frame container ready to include the buttons grid
-frame_container.pack(
-    side=tk.TOP,
-    fill=tk.BOTH
-)
+# --------------------------------------------------------------
+#                           GUI Functions
+# --------------------------------------------------------------
 
-# Buttons images list
-# Every button can assume one of the images in the list, according
-# to the applicaiton logic
-# The button images are named accordingly, in the format b<nn>.png
-b_images = list(tk.PhotoImage(file="images/b%02d.png" % (i+1)) for i in range(max_button_images))
+def create_frame_container():
+    '''
+    Create the frame contained for the GUI buttons grid
 
-# Default image when button is off
-# (on action che image will change with one of the b_images list
-image_off_button = tk.PhotoImage(file="images/bNull.png")
+    :return: the TkInter Frame object
+    '''
+    return tk.Frame(
+        window,
+        highlightcolor='black',
+        highlightbackground='black',
+        background="black",
+        border=10,
+        padx=24,
+        pady=6
+    )
 
-# Number of different function groups, corresponding to different
-# button colors.
-# The button zero (top left corner) changes the functions group
-function_id = 0
+def resize_image_button(name):
+    '''
+    Resize an image according to the button size
 
-# Buttons list
-button = list()
+    The image is loaded from file in its original size, then it is
+    resized and converted to an TkInter PhotoImage ovject for the
+    buttons graphic surface
 
-# Debug flag. Set it to false to disable the debug messages
-_debug = True
+    :param name: The full path image file name
+    :return: The PhotoImage object to be assigned to the button surface
+    '''
+    file = Image.open(name)
+    newsize = (button_size, button_size)
+    btn = file.resize(newsize)
+    return ImageTk.PhotoImage(btn)
 
 def get_button_id(row, col):
     '''
@@ -86,7 +74,7 @@ def get_button_id(row, col):
     and column-by-column
 
     :param row: the selected row
-    :param col:  the selected column
+    :param col: the selected column
     :return: the button id in the list
     '''
     return (row * panel_cols) + col
@@ -120,29 +108,86 @@ def klik(n):
             for i in range(panel_rows):
                 for j in range(panel_cols):
                     button[get_button_id(i, j)].config(image=b_images[function_id])
-        # --- Debug end
+        #--- Debug end
 
 def make_panel():
     '''
-    Create the initial buttons panel in off state
+    Create the initial buttons panel in off state, organized inside
+    a frame of defined rows and columns and associate to every button
+    a corresponding callback fucntion
     '''
+
     # Fill the buttons list with the objects
     for i in range(panel_rows):
         for j in range(panel_cols):
             button.append(
                 tk.Button(frame_container,
-                          image=image_off_button,
-                          command=partial(klik, get_button_id(i, j))),
+                    image=image_off_button,
+                    command=partial(klik, get_button_id(i, j)))
             )
             button[-1].grid(row=i, column=j)
             button[-1].activebackground='black'
-            button[-1].height=button_size + 4
-            button[-1].width=button_size + 4
-            button[-1].border=2
+            button[-1].height=button_size
+            button[-1].width=button_size
+            button[-1].border=8
             button[-1].background='black'
-            button[-1].padx=6
-            button[-1].pady=6
-            button[-1].relief = 'flat'
+            button[-1].relief = 'sunken'
+
+# --------------------------------------------------------------
+#                   Parameters & Constants
+# --------------------------------------------------------------
+
+# Buttons grid size, rows
+panel_rows = 8
+# Buttons grid size, columns
+panel_cols = 8
+# Number of images (shaded colors) a button can have
+max_button_images = 8
+# The max number of fucntion groups (associated to the same number of
+# button colors)
+max_functions = max_button_images
+
+# The size of the square buttons
+button_size = 40
+
+# Number of different function groups, corresponding to different
+# button colors.
+# The button zero (top left corner) changes the functions group
+function_id = 0
+
+# Buttons list
+button = list()
+
+# Debug flag. Set it to false to disable the debug messages
+_debug = True
+
+# --------------------------------------------------------------
+#                   Global calculations
+# --------------------------------------------------------------
+
+# Buttons images list
+# Every button can assume one of the images in the list, according
+# to the applicaiton logic
+# The button images are named accordingly, in the format b<nn>.png
+b_images = list(resize_image_button("images/b%02d.png" % (i+1)) for i in range(max_button_images))
+
+image_off_button = resize_image_button("images/bNull.png")
+
+# The frame that includes all the buttons.
+# The parameters for the border and pads will center the button grid
+# on the screen. Keep them fixed! Should be recalculated if the
+# button size or number of buttons change.
+frame_container = create_frame_container()
+
+# Pack the frame container ready to include the buttons grid
+frame_container.pack(
+    side=tk.TOP,
+    fill=tk.BOTH
+)
+
+# --------------------------------------------------------------
+#                           Application
+# --------------------------------------------------------------
 
 if __name__ == "__main__":
     '''
